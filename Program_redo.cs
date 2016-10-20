7bce8c4e43deeed6ecafe3cb05f5fea0509cad11
Bryan Sory <bryansory@icloud.com>
2016-10-20 17:34:26 -0500

    rovarspraket

---
+++ Program_redo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MainClass{
+    public void Main(){
+        Team myTeam = new Team("Cowboys");
+        List<Player> players = new List<Player>();
+        players.Add(new Player ("Witten", 22));
+        players.Add(new Player ("Jones", 23));
+        players.Add(new Player ("Romo", 5));
+        players.Add(new Player ("Smith", 44));
+        players.Add(new Player ("DelRio", 5));
+    }
+
+public class Team{
+    string name;
+    string Players;
+    int Points;
+    public Team(string name){
+        this.name = name;
+    }
+}   
+public class Player{
+    string name; //getter and setter
+    int points;
+    public Player(){  
+    }
+    public Player (string name, int points){
+        this.name = name;
+        this.points = points;
+    }
+}    
+}    
+
---
+++ rovarspraket.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program {
+
+    public static string rovarspraket(string s){
+        VowelChecker checker = new VowelChecker();
+        string rovarspraket = " " ;
+        char[] list = s // string
+                    .ToLower() // string
+                    .ToCharArray();
+            foreach(char c in list){
+                if (checker.ContainsVowel(c)){
+                    rovarspraket += c.ToString();
+                }
+                else{
+                    rovarspraket += c + "o" + c;
+                }
+            }
+    return rovarspraket;            
+    }                        
+      
+    public class VowelChecker
+{
+    public char[] vowels = new [] { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+
+    public bool ContainsVowel(char c)
+    {
+        foreach (char vowel in vowels)
+        {
+            if (c == vowel)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+    public static void Main(){
+            string x = rovarspraket("airplane");
+            Console.WriteLine(x);
+            Console.WriteLine((rovarspraket("Chris")));
+            Console.WriteLine((rovarspraket("Bryan")));
+            
+    }
+}    
